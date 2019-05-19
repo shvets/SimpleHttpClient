de032e146943a9f1386af301b3e61efaad923497
Alexander Shvets <alexander.shvets@gmail.com>
2019-05-19 17:45:51 -0400

    initial commit

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
   dependencies: [
       // Dependencies declare other packages that this package depends on.
       // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/JohnSundell/Identity", from: "0.2.0")
-    //.package(url: "https://github.com/alexruperez/Tagging", from: "0.1.0")
+    .package(url: "https://github.com/JohnSundell/Identity", from: "0.2.0"),
+    .package(url: "https://github.com/alexruperez/Tagging", from: "0.1.0")
   ],
   targets: [
       // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,7 +31,8 @@ let package = Package(
         name: "SimpleHttpClientTests",
         dependencies: [
           "SimpleHttpClient",
-          "Identity"
+          "Identity",
+          "Tagging"
         ]),
   ]
 )
--- Tests/SimpleHttpClientTests/TaggableTests.swift
+++ Tests/SimpleHttpClientTests/TaggableTests.swift
@@ -1,6 +1,6 @@
 import XCTest
 
-@testable import SimpleHttpClient
+@testable import Tagging
 
 final class TaggableTests: XCTestCase {
 

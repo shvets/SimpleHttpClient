6c7bb61fc743a265fa1ea0840948bf844035ccca
Alexander Shvets <alexander.shvets@gmail.com>
2019-05-19 17:38:56 -0400

    initial commit

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
       // Dependencies declare other packages that this package depends on.
       // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/JohnSundell/Identity", from: "0.2.0")
+    //.package(url: "https://github.com/alexruperez/Tagging", from: "0.1.0")
   ],
   targets: [
       // Targets are the basic building blocks of a package. A target can define a module or a test suite.

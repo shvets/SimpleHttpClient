19d43a53425b3f97ec6692fcc9ba7c558ea6bd90
Alexander Shvets <alexander.shvets@gmail.com>
2019-06-22 13:09:02 -0400

    refactor client

--- Sources/SimpleHttpClient/client/Await.swift
+++ Sources/SimpleHttpClient/client/Await.swift
@@ -2,6 +2,18 @@ import Foundation
 import RxSwift
 
 class Await {
+  static func await<T>(_ handler: @escaping () -> Result<T, ApiError>) throws -> Result<T, ApiError> {
+    let semaphore = DispatchSemaphore.init(value: 0)
+
+    let result: Result<T, ApiError> = handler()
+
+    semaphore.signal()
+
+    _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+
+    return result
+  }
+
   @discardableResult
   static func awaitRx<T>(_ handler: @escaping () -> Observable<T>) throws -> T? {
     var result: T?
@@ -26,18 +38,6 @@ class Await {
     if let error = error {
       throw error
     }
-
-    return result
-  }
-
-  static func await<T: Decodable>(_ handler: @escaping () -> Result<T, ApiError>) throws -> Result<T, ApiError> {
-    let semaphore = DispatchSemaphore.init(value: 0)
-
-    let result: Result<T, ApiError> = handler()
-
-    semaphore.signal()
-
-    _ = semaphore.wait(timeout: DispatchTime.distantFuture)
 
     return result
   }

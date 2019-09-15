49f599a64172f9728d6b0f1f52337c9fead33c44
Alexander Shvets <alexander.shvets@gmail.com>
2019-09-15 15:13:46 -0400

    fix optional issue

--- Sources/SimpleHttpClient/common/Encodable.swift
+++ Sources/SimpleHttpClient/common/Encodable.swift
@@ -3,6 +3,11 @@ import Codextended
 
 extension Encodable {
   public func prettify() throws -> String {
-    return String(data: try encoded(), encoding: .utf8)!
+    if let result = String(data: try encoded(), encoding: .utf8) {
+      return result
+    }
+    else {
+      return "<cannot encode>" 
+    }
   }
 }
